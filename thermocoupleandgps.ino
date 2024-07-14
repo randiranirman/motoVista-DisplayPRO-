4d9aeeef9673d252882d52d6faccdca4395b11f5
randiranirman <164629782+randiranirman@users.noreply.github.com>
2024-07-14 15:17:40 +0530

    Update thermocoupleandgps.ino

--- thermocoupleandgps.ino
+++ thermocoupleandgps.ino
@@ -1,29 +1,70 @@
 #include <TinyGPS++.h>
 #include <LiquidCrystal_I2C.h>
 #include "max6675.h"
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
 
 #define RXD2 16
 #define TXD2 17
 
-HardwareSerial neogps(1); // GPS module serial
+#define WIFI_SSID "UoM_Hostels"
+#define WIFI_PASSWORD ""
+#define API_KEY "AIzaSyDkqHpWBJhHSQCqiJh2MKVNZ5D14nIZTXA"
+#define DATABASE_URL "https://motovista-81aa8-default-rtdb.firebaseio.com"
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+bool signupOK = false;
+
+HardwareSerial neogps(1);  // GPS module serial
 int thermoDO = 19;
 int thermoCS = 23;
 int thermoCLK = 5;
 
-TinyGPSPlus gps; // GPS object
-MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO); // Thermocouple object
+TinyGPSPlus gps;                                      // GPS object
+MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);  // Thermocouple object
 
 // LCD setup
 int lcdColumns = 16;
 int lcdRows = 4;
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  // Address 0x27 for LCD 1
-LiquidCrystal_I2C lcd2(0x26, lcdColumns, lcdRows); // Address 0x26 for LCD 2
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);   // Address 0x27 for LCD 1
+LiquidCrystal_I2C lcd2(0x26, lcdColumns, lcdRows);  // Address 0x26 for LCD 2
 
 void setup() {
   // Start serial communication for debugging
   Serial.begin(115200);
   Serial.println("MAX6675 test");
 
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+  Serial.print("WiFi connected with IP: ");
+  Serial.println(WiFi.localIP());
+
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;  // Corrected database URL
+  Firebase.reconnectWiFi(true);
+
+  config.token_status_callback = tokenStatusCallback;
+
+  Firebase.begin(&config, &auth);
+
+  // Sign up to get a new token
+  if (Firebase.signUp(&config, &auth, "", "")) {
+    Serial.println("Sign up OK");
+    signupOK = true;
+  } else {
+    Serial.printf("Sign up error: %s\n", config.signer.signupError.message.c_str());
+  }
   // Initialize LCDs
   lcd.init();
   lcd2.init();
@@ -49,7 +90,7 @@ void loop() {
   lcd.setCursor(0, 0);
   lcd.print("SPEED:");
   lcd.print(gps.speed.kmph());
-  
+
   // Display temperature on lcd2
   lcd2.setCursor(0, 0);
   lcd2.print("TEMP: ");
@@ -63,7 +104,7 @@ void loop() {
   // Handle GPS data
   while (neogps.available() > 0) {
     char c = neogps.read();
-    Serial.print(c); // Print raw GPS data to Serial Monitor
+    Serial.print(c);  // Print raw GPS data to Serial Monitor
     if (gps.encode(c)) {
       displayInfo();
     }
@@ -74,6 +115,29 @@ void loop() {
   Serial.println(gps.satellites.value());
   Serial.print("Location valid: ");
   Serial.println(gps.location.isValid() ? "Yes" : "No");
+
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 5000)) {
+    sendDataPrevMillis = millis();
+    if (gps.location.isUpdated()) {
+      if (Firebase.RTDB.setFloat(&fbdo, "lat", gps.location.lat())) {
+        Serial.println("latitude data sent successfully.");
+      } else {
+        Serial.println("Failed to send data: " + fbdo.errorReason());
+      }
+      if (Firebase.RTDB.setFloat(&fbdo, "long", gps.location.lng())) {
+        Serial.println("longitude data sent successfully.");
+      } else {
+        Serial.println("Failed to send data: " + fbdo.errorReason());
+      }
+    }
+
+
+    if (Firebase.RTDB.setFloat(&fbdo, "temp", temperature)) {
+      Serial.println("Temperature data sent successfully.");
+    } else {
+      Serial.println("Failed to send temperature data: " + fbdo.errorReason());
+    }
+  }
 
   // Delay between updates
   delay(1000);
@@ -101,7 +165,7 @@ void displayInfo() {
 }
 
 void displayTempStatus(float temperature) {
-  lcd2.setCursor(0, 2); // Set cursor to third row (index 2) of lcd2
+  lcd2.setCursor(0, 2);  // Set cursor to third row (index 2) of lcd2
   if (isnan(temperature)) {
     lcd2.print("STATUS: N/A     ");
   } else if (temperature < 60) {
@@ -115,11 +179,11 @@ void displayTempStatus(float temperature) {
 
 void displaySpeed(float speed) {
   lcd.setCursor(0, 1);
-  int numBars = map(speed, 0, 150, 0, 16); // Map speed to number of bars
+  int numBars = map(speed, 0, 150, 0, 16);  // Map speed to number of bars
   for (int i = 0; i < numBars; i++) {
-    lcd.print((char)255); // Print solid block character for bar
+    lcd.print((char)255);  // Print solid block character for bar
   }
   for (int i = numBars; i < 16; i++) {
-    lcd.print(" "); // Clear remaining part of the graph
+    lcd.print(" ");  // Clear remaining part of the graph
   }
 }
